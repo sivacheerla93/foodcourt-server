c29d08176259be73d9b4239543f35d1a9387335e
Siva Cheerla <sivacheerla@live.com>
2019-04-15 15:24:24 +0530

    Adding item schema

---
+++ models/Item.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    id: Number,
+    foodcourt_id: Number,
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    price: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    availability: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    img: {
+        data: Buffer,
+        contentType: String
+    }
+});
+
+var Item = mongoose.model('Item', ItemSchema, 'item_foodcourt');
+module.exports = Item;
