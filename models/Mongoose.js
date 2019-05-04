4d7c9b8233b87378c3e2e4ef93b9bd82ca6e884e
Siva Cheerla <sivacheerla@live.com>
2019-05-05 00:17:53 +0530

    05/05/2019

--- models/Mongoose.js
+++ models/Mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/foodcourt');
-//mongoose.connect('mongodb://siva:Foodcourt123@ds151066.mlab.com:51066/heroku_xn0071pk');
+//mongoose.connect('mongodb://localhost:27017/foodcourt');
+mongoose.connect('mongodb://siva:Foodcourt123@ds151066.mlab.com:51066/heroku_xn0071pk');
 var db = mongoose.connection;
 
 module.exports = db;
